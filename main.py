6633d8f2779ec69bc556b2d8232edc188e0ce7c1
abrorbek <abrorbekgithub@gmail.com>
2021-03-28 18:38:05 +0500

    sendmessage

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 
 import telegram
-# from telegram import Update
-# from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
 TOKEN="1675228463:AAGWWwMOx3mGstj49_B5NOjFXjgyXFia9xo"
+# "1675228463"
 
 bot=telegram.Bot(token=TOKEN)
 bot_user=bot.getMe()
-print(bot_user.id)
-print(bot_user.first_name)
-print(bot_user.username)
-print(bot_user.can_join_groups)
+# print(bot_user.id)
 
+# print(bot_user.first_name)
+# print(bot_user.username)
+# print(bot_user.can_join_groups)
+
+bot.send_message(chat_id=1091584118,text="111")
 
